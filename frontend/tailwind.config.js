6edb2457615c0d45173e11b96326dc983d16d7b2
NyuKo <kulmurzaev2004@mail.ru>
2026-02-08 02:34:41 +0500

    enhance layout and theming: update app structure, add dark mode support, and improve toolbar styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: 'class', // Включение поддержки темной темы через класс
   theme: {
     extend: {
       colors: {
-        primary: "#3f51b5",
+        primary: {
+          DEFAULT: "#86dad3",
+          dark: "#005a81",
+        },
+        // Добавьте кастомные цвета для фонов
+        bgMain: {
+          light: "#ffffff",
+          dark: "#121212",
+        }
       },
     },
   },
